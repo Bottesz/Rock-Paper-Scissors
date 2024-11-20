e2f9500427bf8951620ce38e226f0264f5daab12
Bottesz <botyaring@gmail.com>
2024-11-20 09:37:25 +0100

    gui kinezet

---
+++ src/kopairollo/Kopairollo.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package kopairollo;
+
+/**
+ *
+ * @author PolgárBotond(SZF_202
+ */
+public class Kopairollo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
